2d30d6e36b42ade12026277047c2fc69548ba326
TheMuver <via.zyn@ya.ru>
2020-11-15 14:14:22 +0300

    create SendMessage method

--- Client/Client.cs
+++ Client/Client.cs
@@ -8,6 +8,7 @@ namespace ClientClassNamespace
     {
         private readonly string _serverAddress;
         private readonly int _port;
+        private NetworkStream _stream;
 
         public ClientClass(string serverAddress, int port)
         {
@@ -18,7 +19,13 @@ namespace ClientClassNamespace
         public void Connect()
         {
             TcpClient client = new TcpClient(_serverAddress, _port);
-            NetworkStream stream = client.GetStream();
+            _stream = client.GetStream();
+        }
+
+        public void SendMessage(string message)
+        {
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            _stream.Write(data, 0, data.Length);
         }
     }
 }
